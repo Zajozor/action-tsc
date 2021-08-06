2352bd91f3cdc84f46220911bb95d82db05f3b49
Zajozor <1835434+Zajozor@users.noreply.github.com>
2021-08-07 00:08:37 +0300

    Use project for path to tsc

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,13 @@ import { exec } from '@actions/exec';
 import { join } from 'path';
 
 async function run() {
-	const subdirectory = getInput('subdirectory');
 	const project = getInput('project');
 	const build = getInput('build');
 	const executable = getInput('executable');
 
-	if (subdirectory) {
-		process.chdir(subdirectory);
-	}
-
 	console.log(`##[add-matcher]${join(__dirname, '..', '.github', 'tsc.json')}`);
 	const args = [
-		`${join(process.cwd(), 'node_modules/.bin', executable)}`,
+		`${join(process.cwd(), project, 'node_modules/.bin', executable)}`,
 		'--noEmit',
 		'--noErrorTruncation',
 		'--pretty',
